26e88b5e965845132d0f6c3c9120cae931e03a7b
HARITHA <54477875+harithab22@users.noreply.github.com>
2021-04-07 13:07:32 -0500

    worked on spring developer tools
    worked on spring developer tools

--- Recipe-project/src/main/java/hari/training/Recipe/project/Controller/IndexController.java
+++ Recipe-project/src/main/java/hari/training/Recipe/project/Controller/IndexController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
     @RequestMapping({"","/","index"})
     public String getIndexpage(){
+        System.out.println("jdkhkdgvzh");
         return "index";
     }
 }
