9054793511db9af1e76f28ff15acf95810ba6ee8
HARITHA <54477875+harithab22@users.noreply.github.com>
2021-04-22 21:45:51 -0500

    jpa-assignments
    jpa-assignments

--- springframework-pet-clinic/src/main/java/com/sf/springframeworkpetclinic/model/pet.java
+++ springframework-pet-clinic/src/main/java/com/sf/springframeworkpetclinic/model/pet.java
@@ -1,11 +1,23 @@
 package com.sf.springframeworkpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name="pets")
 public class pet extends BaseEntity {
+    @Column(name = "name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name="type_id")
     private pettype petType;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @Column(name = "Birth_date")
     private LocalDate dateofbirth;
 
     public String getName() {
--- springframework-pet-clinic/src/main/java/com/sf/springframeworkpetclinic/model/pettype.java
+++ springframework-pet-clinic/src/main/java/com/sf/springframeworkpetclinic/model/pettype.java
@@ -1,6 +1,13 @@
 package com.sf.springframeworkpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="pet_types")
 public class pettype extends BaseEntity {
+    @Column(name = "pet_type")
     private String pettype;
 
     public String getPettype() {
